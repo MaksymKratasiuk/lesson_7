627a65798a3eb828bbfe05067982891eb15dc365
MaksymKratasiuk <makshm1307@gmail.com>
2023-04-13 19:45:18 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace lessom_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = new Array();
+            array.Show();
+            Console.WriteLine("\nEnter information -> ");
+            string info = Console.ReadLine();
+            array.ShowInfo(info);
+        }
+    }
+}
---
+++ Task1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessom_7
+{
+    interface IOutput
+    {
+        void Show();
+        void ShowInfo(string info);
+
+    }
+
+    class Array : IOutput
+    {
+        
+        int[] Arr {get;}
+
+        public Array()
+        {
+            Console.WriteLine("Enter array size ->");
+           Random random = new Random();
+            int size = int.Parse(Console.ReadLine());
+            Arr = new int[size];
+            for(int i = 0;i<size;i++)
+            {
+                Arr[i] = random.Next(-10,10);
+            }
+
+        }
+
+        public void Show()
+        {
+            foreach (var item in Arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public void ShowInfo(string info)
+        {
+            Show();
+            Console.WriteLine("\nInformation : "+info);
+        }
+
+    }
+    
+}
